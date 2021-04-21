44ec3df511831f59e2726dcf30ff065b852bae04
Kazmir_BO <sebastian.kolanowski@op.pl>
2021-04-21 23:04:05 +0200

    Programowanie Obiektowe Mariusz Maciuk

--- Programowanie Obiektowe MM/Dzien 7/Praca domowa.cpp
+++ Programowanie Obiektowe MM/Dzien 7/Praca domowa.cpp
@@ -2,15 +2,25 @@
 #include "data.h"
 int main(int argc, char const *argv[]) {
   int Przesuniecie;
-  Data DataOld(2022,1,1,10,10);
+  char Wybor;
+  Data DataOld(2020,1,1,10,10);
   Data DataNew;
   std::cout<<DataOld<<std::endl;
+  std::cout<<"Co chcesz zrobic?\n"
+  "\t+ Dodawanie,\n"
+  "\t- Odejmowanie,\n\n"
+  "\tWybierz: ";
+  Wybor=getchar();
+  std::cout<<"O jaka wartosc (minuty): ";
   std::cin>>Przesuniecie;
   DataNew.Zmien_czas(Przesuniecie);
-  DataOld=DataOld-DataNew;
-  DataOld=DataOld-DataNew;
-  DataOld.Odejmowanie();
-//  DataOld.Dodawanie();
+  if(Wybor=='+'){
+    DataOld=DataOld+DataNew;
+    DataOld.Dodawanie();
+  }else if(Wybor=='-'){
+    DataOld=DataOld-DataNew;
+    DataOld.Odejmowanie();
+  }
   std::cout<<DataOld<<std::endl;
   std::cout<<std::endl;
   return 0;
