2c7464fa57096ff834129b8f06e1993c2dbde0fa
Kazmir_BO <sebastian.kolanowski@op.pl>
2021-04-25 23:10:09 +0200

    Programowanie Obiektowe Paweł Mikołajczak

---
+++ Programowanie Obiektowe PM/Wykład 6/Kolanowski Sebastian Wykład 6 Zadanie 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//#include <conio.h>
+int main(int argc, char const *argv[]) {
+  std::cout<<"Lola Kwiatek: ";
+  std::cout<<std::fill('-');
+  std::cout<<std::width(10);
+  std::cout<<"500zl"
+  std::cout<<std::endl;
+  return 0;
+}
