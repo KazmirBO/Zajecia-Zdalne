6574e65644d94d705a9eb4b8905ddb273b4a8168
Kazmir <sebastian.kolanowski@op.pl>
2021-05-28 12:58:21 +0200

    Podstawy Programowania Grzegorz Olaszanowski

--- Podstawy Programowania GO/napis2.c
+++ Podstawy Programowania GO/napis2.c
@@ -6,7 +6,9 @@ Data: 08.12.2020
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h> // Pozwala na uzycie toupper. Bez lini 18-20 kod nadal bedzie dzialal, lecz będzie dzielil znaki na male i duze.
+#include <ctype.h>
+// Pozwala na uzycie toupper. Bez lini 18-20 kod nadal bedzie dzialal,
+// lecz będzie dzielil znaki na male i duze.
 int main(){
   char napis[100];
   fflush(stdin);
@@ -16,10 +18,12 @@ int main(){
   printf("\n\tTwoj tekst to: ");
   puts(napis);
   printf("\n");
-  int i,l,temp=0,pow,j,k,ile,a=strlen(napis),x=strlen(napis)-1;
-  for(i=0;i<a;i++) napis[i]=toupper(napis[i]); // Zamiania wszystkich znakow w napis na duze litery
+  int i,l,temp=0,pow,j,ile,a=strlen(napis),x=strlen(napis)-1;
+  for(i=0;i<a;i++) napis[i]=toupper(napis[i]);
+  // Zamiania wszystkich znakow w napis na duze litery
   for(i=0;i<a;i++){
-    if(napis[i]!=napis[x]) temp=1; // Jezeli znak się nie zgadza to nadaje warosc temp=1
+    if(napis[i]!=napis[x]) temp=1;
+    // Jezeli znak się nie zgadza to nadaje warosc temp=1
     x--;
   }
   if(temp==0) printf("\tTwoj wyraz jest palindromem.\n");
@@ -28,14 +32,18 @@ int main(){
     ile=0;
     pow=0;
     for(j=0;j<a;j++){
-      if(napis[j]==napis[i]) ile++; // Powtarzanie się znakow w tekscie
+      if(napis[j]==napis[i]) ile++;
+      // Powtarzanie się znakow w tekscie
     }
     for(l=0;l<=i;l++){
-      if(napis[l]==napis[i]) pow++; // Sprawdza czy znak wystapil wczesniej, aby nie wyswietlic go ponownie
+      if(napis[l]==napis[i]) pow++;
+      // Sprawdza czy znak wystapil wczesniej, aby nie wyswietlic go ponownie
     }
     if(pow==1){
-      if(napis[i]==' ') printf("\n\tZnak <space> wystepuje %d razy.\n",ile); // Testowy dla <space>
-      else printf("\n\tZnak %c wystepuje %2d razy.\n",napis[i],ile); // Wyswietla znak i ilosc występowania
+      if(napis[i]==' ') printf("\n\tZnak <space> wystepuje %d razy.\n",ile);
+      // Testowy dla <space>
+      else printf("\n\tZnak %c wystepuje %2d razy.\n",napis[i],ile);
+      // Wyswietla znak i ilosc występowania
     }
   }
   printf("\n");
