883fc43bc5478e476a391603fa0bb3b4d4207852
Kazmir <sebastian.kolanowski@op.pl>
2021-05-30 13:30:51 +0200

    Podstawy Programowania GO

--- Funkcje/funkcje.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNKCJE_H
-#define FUNKCJE_H
-void Los(int tab[], int n, int lowest, int maximu);
-void Wypisz(int tab[], int n);
-void Babelkowe(int tab[], int n);
-void Insertion(int tab[], int n);
-#endif
