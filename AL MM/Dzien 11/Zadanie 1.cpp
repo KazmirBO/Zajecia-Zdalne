9ae6573dae9f456a658e4ba17638fc606cb3b8b3
Kazmir_BO <sebastian.kolanowski@op.pl>
2021-06-06 19:46:04 +0200

    Algorytmy MM

---
+++ AL MM/Dzien 11/Zadanie 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#define nl std::cout<<std::endl
+void zwiedzaj(int G[n][n], int V[n], int i){
+  V[i]=1;
+  std::cout<<"Badam wierzchołek "<<i<<std::endl;
+  for(int k=0;k<n;k++){
+    if(G[i][k]!=0) zwiedzaj(G,V,k);
+  }
+}
+void szukaj(int G[n][n], int V[n]){
+  for(int i=0;i<n;i++) V[i]=0;
+  for(int i=0;i<n;i++) if(V[i]==0) zwiedzaj(G,V,i);
+}
+int main(int argc, char const *argv[]) {
+  const int n=7;
+  int G[n][n];
+  int V[n];
+  
+  nl;
+  return 0;
+}
---
+++ AL MM/Dzien 11/Zadanie 2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+#define nl std::cout<<std::endl
+void szukaj(int G[n][n], int V[n], int i){
+  queue<int>kolejka;
+  kolejka.push(i);
+  int s;
+  while(!kolejka.empty()){
+    s=kolejka.front();
+    V[s]=1;
+    kolejka.pop();
+    for(int k=0;k<=n;k++){
+      if(G[s][k]!=0){
+        if(V[k]==0){
+          V[k]=1;
+          kolejka.push(k);
+        }
+      }
+    }
+  }
+}
+int main(int argc, char const *argv[]) {
+  const int n=7;
+  int G[n][n];
+  int V[n];
+  nl;
+  return 0;
+}
