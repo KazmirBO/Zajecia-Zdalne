201d39aed8c616c66db90115ca2adc9a2e038750
Kazmir_BO <sebastian.kolanowski@op.pl>
2021-06-13 12:52:35 +0200

    Programowanie Obiektowe PM

---
+++ PO PM/Wykład 9/Kolanowski Sebastian Wykład 9 Zadanie 1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+#define nl std::cout<<std::endl
+class punkt2d{
+public:
+  float x, y;
+  float odl();
+};
+float punkt2d::odl(){
+  return sqrt(pow(x, 2)+pow(y, 2));
+}
+int main(int argc, char const *argv[]) {
+  srand(time(NULL));
+  int n=10;
+  punkt2d ptk[n];
+  float wyniki[n];
+  for(int i=0;i<n;i++){
+    ptk[i].x=rand()%10;
+    ptk[i].y=rand()%10;
+  }
+  float tmp;
+  for(int i=0;i<n;i++) wyniki[i]=ptk[i].odl();
+  for(int i=0;i<n;i++){
+    for(int j=0;j<i;j++){
+      if(wyniki[i]<wyniki[i+1]){
+        tmp=wyniki[i];
+        wyniki[i]=wyniki[i+1];
+        wyniki[i+i]=tmp;
+      }
+    }
+  }
+  std::cout<<"Najwieksza odleglosc wynosi = "<<wyniki[0]<<std::endl;
+  nl;
+  return 0;
+}
---
+++ PO PM/Wykład 9/Kolanowski Sebastian Wykład 9 Zadanie 2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+#define nl std::cout<<std::endl
+class punkt2d{
+private:
+  float x, y;
+public:
+  float odl();
+  void SetValue(int vx, int vy);
+};
+float punkt2d::odl(){
+  return sqrt(pow(x, 2)+pow(y, 2));
+}
+void punkt2d::SetValue(int vx, int vy){
+  x=vx;
+  y=vy;
+}
+int main(int argc, char const *argv[]) {
+  srand(time(NULL));
+  int x, y, n=10;
+  punkt2d ptk[n];
+  float wyniki[n];
+  for(int i=0;i<n;i++){
+    x=rand()%10;
+    y=rand()%10;
+    ptk[i].SetValue(x, y);
+  }
+  float tmp;
+  for(int i=0;i<n;i++) wyniki[i]=ptk[i].odl();
+  for(int i=0;i<n;i++){
+    for(int j=0;j<i;j++){
+      if(wyniki[i]<wyniki[i+1]){
+        tmp=wyniki[i];
+        wyniki[i]=wyniki[i+1];
+        wyniki[i+i]=tmp;
+      }
+    }
+  }
+  std::cout<<"Najwieksza odleglosc wynosi = "<<wyniki[0]<<std::endl;
+  nl;
+  return 0;
+}
