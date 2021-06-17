54ea025659cba339e0c202c81955a6c7cfc6b47a
KazmirBO <sebastian.kolanowski@op.pl>
2021-06-17 10:52:42 +0200

    Praca Własna

--- Praca Własna/C++/wielomian-ulamek.cpp
+++ Praca Własna/C++/wielomian-ulamek.cpp
@@ -3,76 +3,83 @@ Autor: Sebastian Kolanowski
 Temat: Wielomiany
 Data:
   Rozpoczecie: 15:30 19-11-2020
-  Zakonczenie: 23:00 19-11-2020
+  Zakonczenie: xxx
 */
 #include <iostream>
-#include <math.h>
-using namespace std;
-void wielomian(int wielo[],int n,float x){ //sprawdzenie czy do danego x na pewno wychodzi 0
+#include <cmath>
+#include <vector>
+//sprawdzenie czy do danego x na pewno wychodzi 0
+void wielomian(std::vector<int> wielo,int n,float x){
   float wynik=wielo[0];
-  for(int i=1;i<n;i++){
-    wynik=wynik*x+wielo[i];
-  }
-  cout<<"Wartosc wielomianu: "<<wynik<<endl;
+  for(int i=1;i<n;i++) wynik=wynik*x+wielo[i];
+  std::cout<<"Wartosc wielomianu: "<<wynik<<std::endl;
 }
 int main(){
   poczatek:
-  int i=0,temp=1;
-  cout<<"Podaj stopien wielomianu: ";
-  cin>>i; //nadanie stopnia wielomianu
-  int dziel[100],wielo[i+1];
+  int i=0;
+  //nadanie stopnia wielomianu
+  std::cout<<"Podaj stopien wielomianu: ";
+  std::cin>>i;
+  std::vector<float> dziel;
+  std::vector<int> wielo;
+  float war;
   for(int j=0;j<=i;j++){
-    cout<<"Podaj wspolczynnik x"<<i-j<<": "; //testowe - 16x^3-28x^2-4x+3>0
-    cin>>wielo[j];
+    //testowe - 16x^3-28x^2-4x+3>0
+    std::cout<<"Podaj wspolczynnik x"<<i-j<<": ";
+    std::cin>>war;
+    wielo.push_back(war);
   }
-  int pomoc[i+1];
+  float pomoc[i+1];
   for(int z=0;z<=i;z++){
-    if(wielo[z]<0){
-      pomoc[z]=wielo[z]*-1;
-    }
-    else{
-      pomoc[z]=wielo[z];
-    }
-    for(int j=1;j<=pomoc[z];j++){ //sprawdzanie dzielnikow
-	     if(pomoc[z]%j==0){
-         dziel[0]=0; //ewentualne zabezpieczenie z 0 - male problemy
-         dziel[temp]=j;
-         //cout << dziel[temp] << endl;
-         temp++;
-         dziel[temp]=(-j);
-         //cout << dziel[temp] << endl;
-         temp++;
-       }
+    if(wielo[z]<0) pomoc[z]=-wielo[z];
+    else pomoc[z]=wielo[z];
+  }
+  float tmp;
+  for(int j=0;j<=i;j++){
+    for(int k=0;k<j;k++){
+      if(pomoc[j]<pomoc[k]){
+        tmp=pomoc[j];
+        pomoc[j]=pomoc[k];
+        pomoc[k]=tmp;
+      }
     }
   }
-  int lop,spraw;
-  for(int j=0;j<=temp;j++){
-    spraw=0;
-    lop=0;
-    for(int pot=i,h=0;h<=i;h++,pot--){ //sprawdzanie dla jakich wartosci x wielomian ma wartosc 0
-      spraw=spraw+wielo[h]*pow(dziel[j],pot);
+  float j=0.01, temp=pomoc[i];
+  dziel.push_back(0);
+  do{
+    dziel.push_back(j);
+    dziel.push_back(-j);
+    j+=0.01;
+  }while(j<temp);
+  int lop;
+  float spraw, mnoz;
+  for(int j=0;j<=dziel.size();j++){
+    spraw=0; lop=0; mnoz=dziel[j];
+    //sprawdzanie dla jakich wartosci x wielomian ma wartosc 0
+    for(int pot=i,h=0;h<=i;h++,pot--){
+      for(int g=1;g<pot;g++) mnoz*=dziel[j];
+      spraw+=wielo[h]*mnoz;
     }
     if(spraw==0){
-      for(int y=0;y<j;y++){ //petla do zablokowania powtarzania wartosci x
-        if(dziel[y]==dziel[j]){
-          lop++;
-        }
-      }
-      if(lop==0){ //wyswietlenie x
-        cout<<"x= "<<dziel[j]<<endl;
-      }
+      //petla do zablokowania powtarzania wartosci x
+      for(int y=0;y<j;y++) if(dziel[y]==dziel[j]) lop++;
+      //wyswietlenie x
+      if(lop==0) std::cout<<"x= "<<dziel[j]<<std::endl;
     }
   }
   float x;
-  cout<<"Wybiez x: ";
-  cin>>x; //wybor wartosci x
-  int n=sizeof(wielo)/sizeof(wielo[0]);//sizeof - obliczenie ilosci zmiennych w tablicy
-  wielomian(wielo,n,x); //wykonanie funkcji
+  std::cout<<"Wybiez x: ";
+  //wybor wartosci x
+  std::cin>>x;
+  //sizeof - obliczenie ilosci zmiennych w tablicy
+  int n=sizeof(wielo)/sizeof(wielo[0]);
+  //wykonanie funkcji
+  wielomian(wielo,n,x);
   char tn;
-  cout<<"Czy chcesz sprobowac jeszcze raz? (T/N): ";
-  cin>>tn;
+  std::cout<<"Czy chcesz sprobowac jeszcze raz? (T/N): ";
+  std::cin>>tn;
   if(tn=='T' || tn=='t'){
-    cout<<endl<<endl;
+    std::cout<<std::endl<<std::endl;
     goto poczatek;
   }
   return 0;
