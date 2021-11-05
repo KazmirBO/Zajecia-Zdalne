ba25c0dec951409a11312b5ea65a6ca5cbb45dcb
KazmirBO <sebastian.kolanowski@op.pl>
2021-11-05 01:25:23 +0100

    Programowanie Obiektowe PM

--- PO PM/2. Wykład 3/Sebastian Kolanowski Wykład 6 Zadanie 6.cpp
+++ PO PM/2. Wykład 3/Sebastian Kolanowski Wykład 6 Zadanie 6.cpp
@@ -2,11 +2,13 @@
 #include <iterator>
 #include <algorithm>
 #include <vector>
+#include <ctime>
 #define nl std::cout<<std::endl
 int losowa(){
   return rand()%100;
 }
 int main(int argc, char const *argv[]) {
+  srand(time(NULL));
   std::vector<int> v;
   std::generate_n(std::back_inserter(v), 10, losowa);
   std::ostream_iterator<int> out_it (std::cout," ");
--- PO PM/2. Wykład 3/Sebastian Kolanowski Wykład 6 Zadanie 7.cpp
+++ PO PM/2. Wykład 3/Sebastian Kolanowski Wykład 6 Zadanie 7.cpp
@@ -2,6 +2,7 @@
 #include <iterator>
 #include <algorithm>
 #include <vector>
+#include <ctime>
 #define nl std::cout<<std::endl
 int losowa(){
   return rand()%100;
@@ -10,6 +11,7 @@ int mnozenie(int x){
   return x*10;
 }
 int main(int argc, char const *argv[]) {
+  srand(time(NULL));
   std::vector<int> v;
   std::generate_n(std::back_inserter(v), 10, losowa);
   std::ostream_iterator<int> out_it (std::cout," ");
