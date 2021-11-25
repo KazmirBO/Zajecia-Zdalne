9cd96a6ee853b6d3a6ae46f7f465ba8197e58bf7
KazmirBO <sebastian.kolanowski@op.pl>
2021-11-25 10:13:29 +0100

    Programowanie Obiektowe PM

---
+++ PO PM/2. Wykład 4/Zadanie4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <ctime>
+#define nl std::cout<<std::endl
+int main(int argc, char const *argv[]){
+	srand(time(NULL));
+	std::vector<std::string> v1 = {"Jan", "Matejko", "to", "malarz"};
+	std::vector<std::string> v2 = {"Jan", "Matejko", "to", "pisarz"};
+	std::ostream_iterator<std::string> out(std::cout, " ");
+	std::copy(v1.begin(), v1.end(), out);
+	nl;
+	if(std::equal(v1.begin(), v1.end(), v2)) std::cout<<"Zakresy SA podobne."<<std::endl;
+	else std::cout<<"Zakresy NIE sa podobne."<<std::endl;
+	return 0;
+}
