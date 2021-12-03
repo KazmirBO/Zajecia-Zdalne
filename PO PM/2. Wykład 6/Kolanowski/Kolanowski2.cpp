cb53f1c3d58fd4b7a7110cf421003da074d8770e
KazmirBO <sebastian.kolanowski@op.pl>
2021-12-03 10:35:29 +0100

    Programowanie Obiektowe PM

---
+++ PO PM/2. Wykład 6/Kolanowski/Kolanowski2.cpp
@@ -0,0 +1,19 @@
+// Zestaw nr 10
+#include<iostream>
+#include<algorithm>
+#include<iterator>
+#include<list>
+#define nl std::cout<<std::endl
+int main(int argc, char const *argv[]) {
+  std::list<int> lista = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::list<int>::iterator it;
+  for(auto e:lista) std::cout<<e<<" ";
+  nl;
+  it = lista.begin();
+  for(int i=0;i<5;i++) ++it;
+  lista.erase(it);
+  std::transform(lista.begin(), lista.end(), lista.begin(), [](int x){return x+5;});
+  for(auto e:lista) std::cout<<e<<" ";
+  nl;
+  return 0;
+}
---
+++ PO PM/2. Wykład 6/Kolanowski/Kolanowski3.cpp
@@ -0,0 +1,21 @@
+// Zestaw nr 10
+#include<iostream>
+#include<string>
+#include<queue>
+#define nl std::cout<<std::endl
+int main(int argc, char const *argv[]) {
+  std::string S = "abrakadabra";
+  std::priority_queue<char> tekst;
+  char znak;
+  for(int i=0;i<S.size();i++){
+    znak=S[i];
+    tekst.push(znak);
+  }
+  std::cout<<tekst.size()<<std::endl;
+  while(!tekst.empty()){
+    std::cout<<tekst.top()<<" ";
+    tekst.pop();
+  }
+  nl;
+  return 0;
+}
